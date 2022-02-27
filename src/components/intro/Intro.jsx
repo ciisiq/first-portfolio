7ba2a0119f0bb194ac5429a15152b8c0e320075b
ciisiq <cii_3@hotmail.com>
2022-02-27 19:09:47 +0000

    starting mobile responsive

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -7,43 +7,44 @@ export default function Intro() {
     <div className="intro" id="intro">
       <div className="block">
         <div className="right">
-          <div className="social-icons">
-            <a
-              className="i"
-              href="https://www.linkedin.com/in/cintia-siqueira/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <LinkedIn />
-            </a>
-            <a
-              className="i"
-              href="https://github.com/ciisiq"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <GitHub />
-            </a>
-            <a
-              className="i"
-              href="https://www.instagram.com/ciifotos/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <Instagram />
-            </a>
-          </div>
-          <div className="myintro-container">
-            <div className="myintro">
-              <span className="first">
-                Hello I'm <span className="secondname">Cintia</span>
-              </span>
+          <div className="wrapper">
+            <div className="social-icons">
+              <a
+                className="i"
+                href="https://www.linkedin.com/in/cintia-siqueira/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <LinkedIn />
+              </a>
+              <a
+                className="i"
+                href="https://github.com/ciisiq"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <GitHub />
+              </a>
+              <a
+                className="i"
+                href="https://www.instagram.com/ciifotos/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Instagram />
+              </a>
             </div>
-          </div>
-          <div className="message">
-            <span className="message-welcome">Front-End Developer</span>
-
-            <div className="profile-options">
+            <div className="myintro-container">
+              <div className="myintro">
+                <span className="first">
+                  Hello I'm <span className="secondname">Cintia</span>
+                </span>
+              </div>
+            </div>
+            <div className="message">
+              <span className="myrole">Front-End Developer</span>
+            </div>
+            <div className="btn-wrapper">
               <a href="#contact">
                 <button className="btn contactbtn">Contact me</button>
               </a>
@@ -53,7 +54,6 @@ export default function Intro() {
             </div>
           </div>
         </div>
-
         <div className="left">
           <div className="block-neon">
             <h1>Welcome</h1>
