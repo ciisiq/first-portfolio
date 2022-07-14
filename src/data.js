5f88bb7ea65f728c4b25a8de6a4531a009e372fd
ciisiq <cii_3@hotmail.com>
2022-07-14 22:53:46 +0100

    updating the projects

--- src/data.js
+++ src/data.js
@@ -22,28 +22,17 @@ export const data = [
 
   {
     id: "3",
-    title: "DashBoard - Dev Girls",
-    desc: "Project created together with the Dev girls community that I am part of, every month they launch a challenge where online we follow their recommendations and together we create something.",
-    img: "assets/dashboard.png",
-    github: "https://github.com/ciisiq/Dashboard",
-    demo: "#",
-    hastags: "#ReactJs #styled-components #routerDom #storybook",
+    title: "Test News Percayso",
+    desc: "This project is part of a technical interview process for a front-end vacancy",
+    img: "assets/news-app.png",
+    github: "https://github.com/PercaysoRecruitment/pe3-soc_cohort_8-ciisiq",
+    demo: "https://news-percayso-test.netlify.app/",
+    hastags: "#ReactJs #API #PostRequest #interviws",
   },
 
   {
     id: "4",
-    title: "Weather API",
-    desc: "That was a Friday project, famous hackathon. in a trio of 3 people we used a weather API to be able to search any city and country for its current temperature. Observation: The API that we used stoped to work after weeks.",
-    img: "assets/weather.png",
-    github:
-      "https://github.com/SchoolOfCode/w8_d5-hackathon_react-app-with-api-data-32-mjc",
-    demo: "https://happy-mirzakhani-13a0dc.netlify.app/",
-    hastags: "#ReactJs #API #PostRequest #Teamwork",
-  },
-
-  {
-    id: "5",
-    title: "Quote genarator - School of Code",
+    title: "Quote generator - School of Code",
     desc: "This was a project my school coach Jordan, gave me as I was having a hard time understanding what a props was and using fetch resquest. Using an API I created this mini project to strengthen my knowledge.",
     img: "assets/quotes.png",
     github: "https://github.com/SchoolOfCode/react_quote_generator-ciisiq",
@@ -52,17 +41,17 @@ export const data = [
   },
 
   {
-    id: "6",
-    title: "Calculator",
-    desc: "I know everyone creates a calculator, a bootcamp colleague, Eddie, gave me a channel to watch classes and learn new things, that's how I learned to make my first calculator in React.",
-    img: "assets/calculator.png",
-    github: "https://github.com/ciisiq/studying-react",
-    demo: "https://calculator-react-studies.netlify.app/",
-    hastags: "#ReactJs #Calculator #Training #tutorial #study",
+    id: "5",
+    title: "Panda memory game",
+    desc: "a simple memory game, for to show DOM manipulation",
+    img: "assets/page2.png",
+    github: "https://github.com/ciisiq/memory-game",
+    demo: "https://panda-memory-game.netlify.app/",
+    hastags: "#JavaScript #DOM #game #CSS #Grid",
   },
 
   {
-    id: "7",
+    id: "6",
     title: "My first portfolio",
     desc: "of course this whole site would be here haha, it was a big challenge for me to create this site, because with a creative mind I had many challenges. It's still not how I'd like it to be, once I have the knowledge I need it will be.",
     img: "assets/portfolio.png",
